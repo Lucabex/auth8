e7505cd0ff552a703b7f5cc3da95956535b55de7
Luca Bercioux <Lucabex@gmail.com>
2026-08-28 15:29:38 +0100

    Add Data and AppDbContext

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
+using auth8.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
-
+builder.Services.AddDbContext<AppDbContext>(options=>
+options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
